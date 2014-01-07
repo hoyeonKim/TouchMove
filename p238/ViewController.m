f94042e24fa978b32a949290e8c0b222f436c338
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-07 22:00:08 +0900

    TouchMove
    complete

--- p238/ViewController.m
+++ p238/ViewController.m
@@ -8,12 +8,37 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    bool holdingImage;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *ballimage;
 
 @end
 
 @implementation ViewController
-
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *touch = [touches anyObject];
+    CGPoint point =[touch locationInView:self.view];
+    if(CGRectContainsPoint(self.ballimage.frame, point)){
+        holdingImage = YES;
+        self.ballimage.transform = CGAffineTransformMakeScale(1.2, 1.2);
+    }
+}
+-(void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"moved count: %d", [[event allTouches]count]);
+    
+    if(holdingImage){
+        UITouch *touch = [touches anyObject];
+        CGPoint point = [touch locationInView:self.view];
+        self.ballimage.center = point;
+    }
+}
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    if(holdingImage){
+        self.ballimage.transform = CGAffineTransformIdentity;
+        holdingImage = NO;
+    }
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
